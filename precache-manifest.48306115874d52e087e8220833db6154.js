1dcd20f493bc50111ec1295955a31331fee16043
José Antônio Rapozo <joseantonio.rapozo@gmail.com>
2022-02-02 20:22:29 -0300

    Adding files

---
+++ precache-manifest.48306115874d52e087e8220833db6154.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806e7f1dabbd8593a693c345ad6ede90",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "056511dc8609b9669cfb",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056511dc8609b9669cfb",
+    "url": "/home/static/js/main.d577b759.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
